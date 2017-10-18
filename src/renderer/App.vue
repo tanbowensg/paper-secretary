589bc569321e89bd3cf15c6c77e4a7d21cd90526
tanbowen <tanbowensg@gmail.com>
2017-10-18 10:52:10 +0800

    init

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'paper-secretary'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
